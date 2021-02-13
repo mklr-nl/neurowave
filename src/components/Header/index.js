21f6553c07e119be458f1273e1485e1d23e384dd
mklr-nl <info@mklr.nl>
2021-02-13 14:02:29 +0100

    wip

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,26 @@
 import "./index.scss";
 
-const burger = document.getElementById("openburger");
+const burger = document.querySelector(".mobileMenu__open");
+const mobileMenuOpen = document.querySelector(".mobileMenu__open");
+const mobileMenuClose = document.querySelector(".mobileMenu__close");
+
+const menuList = document.querySelector(".Menu__list");
 
 
-burger.addEventListener("click", function () {
-    console.log("Hello")
+mobileMenuOpen.addEventListener("click", function () {
+    menuList.classList.add("active");
+    setTimeout(function () {
+        menuList.classList.add("menuDown")
+        mobileMenuOpen.classList.add("notActive")
+        mobileMenuClose.classList.add("active")
+    }, 1)
+})
+
+mobileMenuClose.addEventListener("click", function () {
+    menuList.classList.remove("menuDown")
+    mobileMenuClose.classList.remove("active")
+    mobileMenuOpen.classList.remove("notActive")
+    setTimeout(function () {
+        menuList.classList.remove("active");
+    }, 100)
 })
