c4e4d84152da3a85bc66fe2c39957aa3235dbcc1
mklr-nl <info@mklr.nl>
2021-02-14 17:41:02 +0100

    wip

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,26 +1,29 @@
 import "./index.scss";
 
-const burger = document.querySelector(".mobileMenu__open");
-const mobileMenuOpen = document.querySelector(".mobileMenu__open");
-const mobileMenuClose = document.querySelector(".mobileMenu__close");
-
-const menuList = document.querySelector(".Menu__list");
 
 
-mobileMenuOpen.addEventListener("click", function () {
-    menuList.classList.add("active");
-    setTimeout(function () {
-        menuList.classList.add("menuDown")
-        mobileMenuOpen.classList.add("notActive")
-        mobileMenuClose.classList.add("active")
-    }, 1)
-})
+// const menuBtn = document.querySelector(".menu-icon span");
+// const searchBtn = document.querySelector(".search-icon");
+// const cancelBtn = document.querySelector(".cancel-icon");
+// const items = document.querySelector(".nav-items");
+// const form = document.querySelector("form");
 
-mobileMenuClose.addEventListener("click", function () {
-    menuList.classList.remove("menuDown")
-    mobileMenuClose.classList.remove("active")
-    mobileMenuOpen.classList.remove("notActive")
-    setTimeout(function () {
-        menuList.classList.remove("active");
-    }, 100)
-})
+// menuBtn.onclick = () => {
+//     items.classList.add("active");
+//     menuBtn.classList.add("hide");
+//     searchBtn.classList.add("hide");
+//     cancelBtn.classList.add("show");
+// }
+// cancelBtn.onclick = () => {
+//     items.classList.remove("active");
+//     menuBtn.classList.remove("hide");
+//     searchBtn.classList.remove("hide");
+//     cancelBtn.classList.remove("show");
+//     form.classList.remove("active");
+//     cancelBtn.style.color = "#ff3d00";
+// }
+// searchBtn.onclick = () => {
+//     form.classList.add("active");
+//     searchBtn.classList.add("hide");
+//     cancelBtn.classList.add("show");
+// }
