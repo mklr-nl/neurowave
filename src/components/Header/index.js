1b6def7c20cbee074610985defd07b60ee6b70bd
mklr-nl <info@mklr.nl>
2021-02-19 15:35:20 +0100

    wip

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,7 @@ import "./index.scss";
 const burgerOpen = document.querySelector(".Nav__burger__icon__open");
 const burgerClose = document.querySelector(".Nav__burger__icon__close");
 const mobMenu = document.querySelector(".Extra__menu");
+const infoBox = document.querySelector(".Info__title");
 
 //! for the intro part image opacity to 50%
 const introImg = document.querySelector(".Intro__mob__img");
@@ -15,6 +16,7 @@ function moveMenu() {
     burgerClose.classList.toggle("notActive");
     //! for the intro part image opacity to 50%
     introImg.classList.toggle("opacity50");
+    infoBox.classList.toggle("Transform0")
 }
 
 on("click", ".Nav__burger__icon", function(event){
