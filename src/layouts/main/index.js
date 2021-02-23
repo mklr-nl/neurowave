4f3570789344e55e6eae060aacc7e5b25503bfd0
mklr-nl <info@mklr.nl>
2021-02-23 17:54:57 +0100

    wip

--- src/layouts/main/index.js
+++ src/layouts/main/index.js
@@ -5,3 +5,8 @@ import "src/components/Infoboxes";
 import "src/components/Observer";
 import "src/utils/grid/_index.scss";
 import "./index.scss";
+
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
