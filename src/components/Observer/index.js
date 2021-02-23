96670650286a6230146a8244c9907046de612f06
mklr-nl <info@mklr.nl>
2021-02-23 17:02:11 +0100

    wip

--- src/components/Observer/index.js
+++ src/components/Observer/index.js
@@ -1,23 +1,30 @@
 import "./index.scss";
 
 
-const sectionOne = document.querySelector(".section1");
-const sections = document.querySelectorAll(".Section");
+const sections = document.querySelectorAll(".Info__boxes__box");
+// const sectionOne = document.querySelector(".section4");
+
+const box1 = sections[0];
+const box2 = sections[1];
+const box3 = sections[2];
 
 const options = {
-    root: null,
-    treshhold: 0,
-    rootMargin: "0px" 
+  root: null,
+  threshold: 0.1,
+  rootMargin: "0px"
 };
 
-const observer = new IntersectionObserver(function(entries, observer) {
-    entries.forEach(entry => {
-        console.log(entry.target);
-        
-    });
+const observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    }
+    entry.target.classList.toggle("inverse");
+    observer.unobserve(entry.target);
+    console.log(entry.target);
+  });
 }, options);
 
-sections.forEach(section => {
-    observer.observe(section);
-})
-// observer.observe(sectionOne);
+sections.forEach((section) => {
+  observer.observe(section);
+});
