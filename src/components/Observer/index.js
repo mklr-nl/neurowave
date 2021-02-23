6cb5506bcc8f09a7ad43cad520ee8e11061979af
mklr-nl <info@mklr.nl>
2021-02-23 17:21:48 +0100

    wip

--- src/components/Observer/index.js
+++ src/components/Observer/index.js
@@ -1,27 +1,27 @@
 import "./index.scss";
 
 
-const sections = document.querySelectorAll(".Info__boxes__box");
-// const sectionOne = document.querySelector(".section4");
+// const sections = document.querySelectorAll(".Info__boxes__box");
+// // const sectionOne = document.querySelector(".section4");
 
 
-const options = {
-  root: null,
-  threshold: 0.2,
-  rootMargin: "0px"
-};
+// const options = {
+//   root: null,
+//   threshold: 0.2,
+//   rootMargin: "0px"
+// };
 
-const observer = new IntersectionObserver(function (entries, observer) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      return;
-    }
-    entry.target.classList.toggle("inverse");
-    observer.unobserve(entry.target);
-    console.log(entry.target);
-  });
-}, options);
+// const observer = new IntersectionObserver(function (entries, observer) {
+//   entries.forEach(entry => {
+//     if (!entry.isIntersecting) {
+//       return;
+//     }
+//     entry.target.classList.toggle("inverse");
+//     observer.unobserve(entry.target);
+//     console.log(entry.target);
+//   });
+// }, options);
 
-sections.forEach((section) => {
-  observer.observe(section);
-});
+// sections.forEach((section) => {
+//   observer.observe(section);
+// });
